67d748f8d0671f5c2991a6629264a95ac8480545
Thomas Veerman <thomas.veerman@wanwire.net>
2023-11-22 11:10:54 +0000

    XrayKit Release 1.8.6
    Signed-off-by: GitHub Actions Bot <thomas.veerman@wanwire.net>

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+  name: "XrayKit",
+  platforms: [.iOS(.v16), .macOS(.v14), .macCatalyst(.v16), .tvOS(.v17)],
+  products: [
+    .library(
+        name: "XrayKit",
+        targets: ["XrayKit"]
+    )
+  ],
+  targets: [
+    .target(
+        name: "XrayKit",
+        dependencies: ["XrayKitC", "LibXray"]
+    ),
+    .target(
+        name: "XrayKitC",
+        publicHeadersPath: "."
+    ),
+    .binaryTarget(
+        name: "LibXray",
+	url: "https://github.com/Wanwire/FloxcoreXrayKit/releases/download/1.8.6/LibXray.xcframework.zip",
+	checksum: "7b1e49b601b9e3814fa9dff256999029aeadd11d58ce65d9531a22b18cac6e15"
+    )
+  ]
+)
