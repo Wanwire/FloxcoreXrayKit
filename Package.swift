fa78af5fb550019379db35898eccf521b687816c
Thomas Veerman <thomas.veerman@wanwire.net>
2026-03-11 14:41:44 +0000

    XrayKit Release 2026.02.06
    Signed-off-by: GitHub Actions Bot <thomas.veerman@wanwire.net>

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "LibXrayGo",
-        url: "https://github.com/Wanwire/FloxcoreXrayKit/releases/download/2025.10.15/LibXrayGo.xcframework.zip",
-        checksum: "2e6566b31cc4735cbf472ab9bdf31fcdc4f9b5cb1818fc5e94e8e42132af266f"
+        url: "https://github.com/Wanwire/FloxcoreXrayKit/releases/download/2026.02.06/LibXrayGo.xcframework.zip",
+        checksum: "66c9db963ceb625b9d5583148b8512b7e5dff4481c579b9f1d7c3ac5d16dcdd9"
     )
   ]
 )
